7aa20427de0c59b6b857b36a7255edec0de61ee4
CofCat456 <a25690756@gmail.com>
2023-01-14 18:55:26 +0800

    docs: 🐜 第三週主線任務

---
+++ week_3/src/components/Login.vue
@@ -0,0 +1,129 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import { useForm } from '@vorms/core';
+import { yupResolver } from '@vorms/resolvers/yup';
+import * as yup from 'yup';
+
+import Swal from 'sweetalert2';
+
+import { loginApi } from '../api';
+
+const router = useRouter();
+
+const schema = yup.object().shape({
+  account: yup.string().email('請輸入正確的 Email 格式 ㅍ_ㅍ!!').required('請輸入 Email !!'),
+  password: yup.string('').min(6, '密碼長度不得小於 6').required('請輸入密碼!!'),
+});
+
+const { errors, register, handleSubmit, handleReset } = useForm({
+  initialValues: {
+    account: '',
+    password: '',
+  },
+  validate: yupResolver(schema),
+  onSubmit(values) {
+    const { account, password } = values;
+    loginApi({
+      username: account,
+      password,
+    })
+      .then((res) => {
+        const {
+          data: { success, token, expired },
+        } = res;
+        if (success) {
+          Swal.fire({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1500,
+            timerProgressBar: true,
+            icon: 'success',
+            title: '登入成功 (*‘ v`*)',
+          });
+          document.cookie = `token=${token};expires=${new Date(expired)};`;
+          router.push({ name: 'ProductList' });
+        }
+      })
+      .catch((err) => {
+        const {
+          response: { message },
+        } = err;
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1500,
+          timerProgressBar: true,
+          icon: 'error',
+          title: '登入失敗',
+          text: message,
+        });
+      });
+  },
+});
+
+const { value: account, attrs: accountFieldAttrs } = register('account');
+const { value: password, attrs: passwordFieldAttrs } = register('password');
+</script>
+
+<template>
+  <section class="container position-absolute top-50 start-50 translate-middle text-center">
+    <div class="row justify-content-center">
+      <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+      <div class="col-8">
+        <form class="form-signin" novalidate @submit="handleSubmit" @reset="handleReset">
+          <div class="form-floating mb-3">
+            <input
+              id="username"
+              type="email"
+              class="form-control"
+              placeholder="name@example.com"
+              v-bind="accountFieldAttrs"
+              v-model="account"
+            />
+            <label for="username">Email address</label>
+            <div class="feedback">{{ errors.account }}</div>
+          </div>
+          <div class="form-floating">
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              placeholder="Password"
+              v-bind="passwordFieldAttrs"
+              v-model="password"
+            />
+            <label for="password">Password</label>
+            <div class="feedback">{{ errors.password }}</div>
+          </div>
+          <button class="btn btn-lg btn-primary w-100 mt-3" type="submit">登入</button>
+          <button class="btn btn-lg btn-secondary w-100 mt-3" type="reset">重置</button>
+        </form>
+      </div>
+    </div>
+    <p class="mt-5 mb-3 text-muted">&copy; 2021~∞ - 六角學院</p>
+  </section>
+</template>
+
+<style scoped>
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+
+.feedback {
+  width: 100%;
+  margin-top: 0.25rem;
+  font-size: 0.875em;
+  color: #dc3545;
+}
+</style>
---
+++ week_3/src/components/ProductList.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="container-fruid">
+    <div class="row justify-content-center py-5">
+      <div class="col-md-8">
+        <h2>產品列表</h2>
+        <table class="table table-hover mt-4 align-middle text-center">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">產品名稱</th>
+              <th scope="col">原價</th>
+              <th scope="col">售價</th>
+              <th scope="col">是否啟用</th>
+              <th scope="col">查看細節</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(product, index) in products" :key="product.id">
+              <td>{{ index + 1 }}</td>
+              <td>{{ product.title }}</td>
+              <td>
+                {{ product.origin_price }}
+              </td>
+              <td>
+                {{ product.price }}
+              </td>
+              <td>
+                <span :class="`text-${product.is_enabled ? 'success' : 'muted'}`">{{
+                  product.is_enabled ? '啟用' : '未啟用'
+                }}</span>
+              </td>
+              <td>
+                <button type="button" class="btn btn-primary" @click="watchProductDetail(product)">
+                  查看細節
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p>
+          目前有 <span>{{ products.length }}</span> 項產品
+        </p>
+      </div>
+    </div>
+    <button type="button" class="btn btn-outline-secondary logoutBtn" @click="logout">登出</button>
+    <ProductModal ref="productModal" v-bind="tempProduct" />
+  </div>
+</template>
+
+<script>
+import ProductModal from './ProductModal.vue';
+import Swal from 'sweetalert2';
+
+import { setDefaultAuth, getProductApi, checkLoginApi, logoutApi } from '../api';
+
+export default {
+  components: {
+    ProductModal,
+  },
+  data() {
+    return {
+      products: [],
+      tempProduct: {},
+    };
+  },
+  methods: {
+    expiredToken() {
+      Swal.fire({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: true,
+        icon: 'error',
+        title: 'token 已過期 (′゜ω。‵)',
+      });
+      this.$router.push({ name: 'Login' });
+    },
+    checkLogin() {
+      checkLoginApi()
+        .then(() => {
+          this.getProduct();
+        })
+        .catch(() => {
+          this.$router.push({ name: 'Login' });
+        });
+    },
+    logout() {
+      logoutApi().then(() => {
+        Swal.fire({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 1500,
+          timerProgressBar: true,
+          icon: 'success',
+          title: '登出成功 ฅ●ω●ฅ',
+        });
+        this.$router.push({ name: 'Login' });
+      });
+    },
+    getProduct() {
+      getProductApi()
+        .then((res) => {
+          const {
+            data: { products },
+          } = res;
+          this.products = Object.values(products);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    watchProductDetail(item) {
+      this.tempProduct = item;
+      this.$refs.productModal.showModal();
+    },
+  },
+  mounted() {
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, '$1');
+
+    if (!token) {
+      this.expiredToken();
+      return;
+    }
+
+    setDefaultAuth(token);
+    this.checkLogin();
+  },
+};
+</script>
+
+<style>
+.logoutBtn {
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+}
+</style>
