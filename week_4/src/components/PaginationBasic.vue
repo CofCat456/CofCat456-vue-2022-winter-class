583a9582561170dc9046800d8f424cb3d1b22fcf
CofCat456 <a25690756@gmail.com>
2023-01-17 01:35:19 +0800

    feat: 🐈 pagination

---
+++ week_4/src/components/PaginationBasic.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-center">
+      <li class="page-item disabled">
+        <a class="page-link">Previous</a>
+      </li>
+      <li class="page-item" v-for="page in pages" :key="page">
+        <a class="page-link" href="#">{{ page }}</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" href="#">Next</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: {
+    pages: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      modal: {}
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style></style>
