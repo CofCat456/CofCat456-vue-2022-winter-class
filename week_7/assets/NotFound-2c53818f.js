161f29d1bdd66c18d3edb056f8e9876bcf5c6304
CofCat456 <a25690756@gmail.com>
2023-03-05 19:26:10 +0800

    update 2023-03-05 19:26:10

---
+++ week_7/assets/NotFound-2c53818f.js
@@ -0,0 +1,1 @@
+import{_ as t,o as c,c as s,e}from"./index-b8bbc98a.js";const o={},n={class:"container-fruid"},a=e("div",{style:{height:"calc(100vh - 56px)"},class:"row justify-content-center align-items-center"},[e("div",{class:"text-center"},[e("h1",{class:"mb-3"},"這是 404 頁面你知道的")])],-1),r=[a];function i(_,d){return c(),s("div",n,r)}const f=t(o,[["render",i]]);export{f as default};
